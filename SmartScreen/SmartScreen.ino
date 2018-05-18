88d386a5b6d23b053f4e78727fff4e329a0bac67
萬俊瑋 <wei840222@gmail.com>
2018-05-18 19:01:59 +0800

    add smart screen src

---
+++ SmartScreen/SmartScreen.ino
@@ -0,0 +1,21 @@
+#include <Servo.h>
+
+int motorA = 0;
+
+Servo servoA;
+
+void setup()
+{
+  Serial.begin(9600);
+  servoA.attach(10);
+}
+
+void loop()
+{
+  motorA = servoMove(servoA, 30, motorA);
+  Serial.println(motorA);
+  delay(5000);
+  if (motorA > 180)
+    motorA = 0;
+}
+
