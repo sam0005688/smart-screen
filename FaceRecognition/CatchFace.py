93d2e84b6212d3a868e3a0b0c3c76de886f64d60
萬俊瑋 <wei840222@gmail.com>
2018-06-09 17:39:19 +0800

    前後 左右追蹤

--- FaceRecognition/CatchFace.py
+++ FaceRecognition/CatchFace.py
@@ -6,11 +6,12 @@ import serial
 cascPath = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascPath)
 
-video_capture = cv2.VideoCapture(0)
+video_capture = cv2.VideoCapture(1)
 anterior = 0
 ser = serial.Serial("COM5", 9600, timeout=2) # Establish the connection on a specific port
 
-dataCount, data = 0, 0
+dataCount, mx, my = 0, 0, 0
+dalay = 10
 
 while True:
     if not video_capture.isOpened():
@@ -35,22 +36,36 @@ while True:
     # Draw a rectangle around the faces
     if len(faces) == 1:
         x, y, w, h = faces[0][0], faces[0][1], faces[0][2], faces[0][3]
-        print(x+w/2-320, -(y+h/2-240))
-        if x+w/2-320 > 60:
-            ser.write(bytes('r', encoding = 'utf8'))
-        if x+w/2-320 < -60:
-            ser.write(bytes('l', encoding = 'utf8'))
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        mx = x+w/2-320
+        my = -(y+h/2-240)
+        print(mx, my)
+        if mx > 60:
+            ser.write(bytes('l', encoding = 'utf8'))
+        if mx < -60:
+            ser.write(bytes('r', encoding = 'utf8'))
+        # if my > 30:
+        #     ser.write(bytes('d', encoding = 'utf8'))
+        # if my < -30:
+        #     ser.write(bytes('u', encoding = 'utf8'))
 
+        # if dataCount == dalay:
+        #     ser.flushOutput()
+        #     mx /= dalay
+        #     my /= dalay
+        #     dataCount = 0
+        #     print(mx, my)
+            
+        # else:
+        #     mx += x+w/2-320
+        #     my += -(y+h/2-240)
+        #     dataCount += 1
     # Display the resulting frame
     cv2.imshow('Video', frame)
-
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-    # Display the resulting frame
-    cv2.imshow('Video', frame)
 
 # When everything is done, release the capture
 video_capture.release()
